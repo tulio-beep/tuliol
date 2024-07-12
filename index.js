f09c53b40ea2e022b9fd979518d5cd681cc8ab3d
Tuliol <tuliovin@outlook.com>
2024-07-12 16:30:46 -0300

    ajuste nome snake

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function update() {
     // A cobra cresce
     // Gera uma nova posição para a comida
     pontuacao += 10;
-    tittle.innerText = `SNAKE - Pontuação: ${pontuacao}`;
+    tittle.innerText = `Snake - Pontuação: ${pontuacao}`;
     snake.size++;
     food = {
       x: parseInt(Math.random() * columns),
@@ -189,7 +189,7 @@ function update() {
 
 // Loop principal do jogo
 function start() {
-  document.getElementById("tittle").innerText = "SNAKE"
+  document.getElementById("tittle").innerText = "Snake"
   // Posição inicial da cobra
   x_ = parseInt(Math.random() * columns);
   y_ = parseInt(Math.random() * rows);
